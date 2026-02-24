40b34b8072466c897a026f7023f58c9b7afb3bfb
حسن محمد ابراهيم عبد العزيز الحلو ( 423232057 ) <423232057@sha.edu.eg>
2026-02-24 02:14:42 +0200

    add admin reset password , confirm reset

--- GraduationProject/Dtos/ConfirmResetDto.cs
+++ GraduationProject/Dtos/ConfirmResetDto.cs
@@ -2,7 +2,7 @@
 {
     public class ConfirmResetDto
     {
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public string Code { get; set; }
         public string NewPassword { get; set; }
     }
